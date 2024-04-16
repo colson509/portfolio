81b45e6680bb2cc3b825d779968a9fe6c89a9ad0
Colson <colsonfaustin69@gmail.com>
2024-04-15 23:56:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+    function toggleVisibility(sectionId) {
+        var section = document.getElementById(sectionId);
+        if (section.style.display === 'none') {
+            section.style.display = 'block';
